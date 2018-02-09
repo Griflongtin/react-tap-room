c481ad52e45950b1071e54c347401ee5a6bf4644
Griffin P Longtin <griflongtin@live.com>
2018-02-09 11:09:14 -0800

    add a link in nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,14 @@
 import React from 'React';
+import { Link } from 'react-router-dom';
 
 function Nav() {
+  const linkStyle = {
+    textDecoration: 'none',
+    color: 'black'
+  };
   return (
     <div>
+      <Link to="/" style={linkStyle}>Taps</Link>
     </div>
   );
 }
