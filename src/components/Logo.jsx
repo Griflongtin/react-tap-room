736d5c675f8ddcd1ed0357c31e85fb4e5a72d9e3
Griffin P Longtin <griflongtin@live.com>
2018-02-09 10:58:22 -0800

    add import for img in Logo component

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,9 @@
 import React from 'React';
-
+import ReactBeerLogo from '../Assets/beer-logo.png';
 function Logo() {
   return (
     <div>
-
+      <img src={ReactBeerLogo} />
     </div>
   );
 }
