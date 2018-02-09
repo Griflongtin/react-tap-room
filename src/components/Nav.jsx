7509e8070deff4b0b8497fdb135a3323d15f1041
Griffin P Longtin <griflongtin@live.com>
2018-02-09 12:14:29 -0800

    add style to Nav component

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,28 +4,31 @@ import { Link } from 'react-router-dom';
 function Nav() {
   const linkStyle = {
     textDecoration: 'none',
-    color: 'black'
+    color: '#fff'
   };
   return (
     <div>
       <style jsx>{`
           ul {
             display: flex;
+            background-color: gray;
+          }
+          li {
+            list-style: none;
+            padding: 10px;
+            text-align: center;
+            width: 145px;
+            border: 2px dashed red;
+          }
+          li:hover {
+            background-color: brown;
           }
       `}</style>
       <ul>
-        <li>
-          <Link to="/" style={linkStyle}>Taps</Link>
-        </li>
-        <li>
-          <Link to="/" style={linkStyle}>Input New Keg</Link>
-        </li>
-        <li>
-          <Link to="/" style={linkStyle}>Keg Inventory</Link>
-        </li>
-        <li>
-          <Link to="/aboutUs" style={linkStyle}>About Us</Link>
-        </li>
+        <Link to="/" style={linkStyle}><li>Taps</li></Link>
+        <Link to="/" style={linkStyle}><li>Input New Keg</li></Link>
+        <Link to="/" style={linkStyle}><li>Keg Inventory</li></Link>
+        <Link to="/aboutUs" style={linkStyle}><li>About Us</li></Link>
       </ul>
     </div>
   );
