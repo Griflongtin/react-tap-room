e28f5239487ae08a9d2278dea2904443503cead8
Griffin P Longtin <griflongtin@live.com>
2018-02-09 17:09:44 -0800

    add inputForm component with form

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 import Tap from './Tap';
 import AboutUs from './AboutUs';
 import Inventory from './Inventory';
+import InputForm from './InputForm';
 
 function Body() {
   return (
@@ -12,6 +13,7 @@ function Body() {
         <Route exact path='/' component={ Tap } />
         <Route path='/aboutUs' component={ AboutUs } />
         <Route path='/inventory' component={ Inventory } />
+        <Route path='/inputForm' component={ InputForm } />
       </Switch>
     </div>
   );
---
+++ src/components/InputForm.jsx
@@ -0,0 +1,47 @@
+import React from 'React';
+
+function InputForm() {
+  return (
+    <div className="inputFormDiv">
+      <style jsx>{`
+          .inputFormDiv {
+            display: flex:
+          }
+      `}</style>
+      <div>
+        <h1>Input a new keg</h1>
+      </div>
+      <form>
+        <div>
+          <label>
+            Name:
+            <input type="text" placeholder="Name Of Beer" />
+          </label>
+          <label>
+            Brewer:
+            <input type="text" placeholder="Brewery" />
+          </label>
+          <label>
+            Price:
+            <input type="number" />
+          </label>
+          <lable>
+            ABV:
+            <input type="number" />
+          </lable>
+          <lable>
+            Brewer Logo link:
+            <input type="text" />
+          </lable>
+          <lable>
+            Quantity
+            <input type="number" />
+          </lable>
+          <button type="onSubmit">Add Keg</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default InputForm;
